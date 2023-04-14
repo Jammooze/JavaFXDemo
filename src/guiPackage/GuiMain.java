fd6a00f2b6aecda0ed4914cb1f16cb7eaa3339d8
Jammooze <Jammooze120@gmail.com>
2023-04-13 21:15:31 -0700

    Adding 3x3 Colored Grid Option

--- src/guiPackage/GuiMain.java
+++ src/guiPackage/GuiMain.java
@@ -1,3 +1,10 @@
+/**
+*  @author Nguyen James 
+*          CS 2012-03
+*          Activity - Intro to JavaFX
+*          GUIMain.java
+*/
+
 package guiPackage;
 
 import javafx.application.Application;
@@ -13,16 +20,9 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-// make sure to add these lines to the
-// module-info.java file
-//	 requires javafx.controls;
-//   requires javafx.base;
-//   requires javafx.graphics;
-//   exports app; // <-name of the package
-
 public class GuiMain extends Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // Launches GUI Application
 		Application.launch(args);
 	}
 
@@ -73,6 +73,58 @@ public class GuiMain extends Application {
             Scene scene = new Scene(vbox, 500, 400); // (vbox, length, width)
             primaryStage.setScene(scene);
             primaryStage.show();
+            
+            // Initialize new Button for creating a new window
+            Button newWindowButton = new Button("Open Color Grid\n        Window");
+            
+            // Create new GUI Window upon newWindowButton Click <-- 3x3 COLOR GRID WINDOW
+            newWindowButton.setOnAction(e -> {
+                // Creating new Stage & Scene
+                Stage newWindow = new Stage();
+                
+                // Initialize two new VBox & HBox OBJs
+                VBox newVBox = new VBox(); // Vertical
+                newVBox.setSpacing(10);
+                
+                HBox HBox1 = new HBox(); // 3 Horizontal for 3 rows 
+                HBox1.setSpacing(10);
+                HBox HBox2 = new HBox();
+                HBox2.setSpacing(10);
+                HBox HBox3 = new HBox();
+                HBox3.setSpacing(10);
+                
+                Text newText = new Text("COLOR GRID");
+                
+                // Initialize 9 Rectangle OBJs for 3x3 Colored Grid
+                Rectangle square1 = new Rectangle(50, 50, Color.LIGHTPINK); // 1st Row
+                Rectangle square2 = new Rectangle(50, 50, Color.LIGHTPINK);
+                Rectangle square3 = new Rectangle(50, 50, Color.LIGHTPINK);
+                Rectangle square4 = new Rectangle(50, 50, Color.LIGHTBLUE); // 2nd Row
+                Rectangle square5 = new Rectangle(50, 50, Color.LIGHTPINK);
+                Rectangle square6 = new Rectangle(50, 50, Color.LIGHTBLUE);
+                Rectangle square7 = new Rectangle(50, 50, Color.LIGHTPINK); // 3rd Row
+                Rectangle square8 = new Rectangle(50, 50, Color.LIGHTPINK);
+                Rectangle square9 = new Rectangle(50, 50, Color.LIGHTBLUE);
+                
+                HBox1.getChildren().addAll(square1, square2, square3); // Adding 1st row to HBox1
+                HBox1.setAlignment(Pos.CENTER);
+                HBox2.getChildren().addAll(square4, square5, square6); // Adding 2nd row to HBox2
+                HBox2.setAlignment(Pos.CENTER);
+                HBox3.getChildren().addAll(square7, square8, square9); // Adding 3rd row to HBox3
+                HBox3.setAlignment(Pos.CENTER);
+                
+                newVBox.getChildren().addAll(newText, HBox1, HBox2, HBox3); // Stacking to newVBox 
+                newVBox.setAlignment(Pos.CENTER);
+                
+                // Output GUI Window
+                Scene newScene = new Scene(newVBox, 300, 300);
+                newWindow.setScene(newScene);
+                newWindow.show();
+            });
+            
+            // Add the new button to the HBox
+            vbox.getChildren().add(newWindowButton);
+            
         } catch(Exception e) {
             e.printStackTrace();
         }
